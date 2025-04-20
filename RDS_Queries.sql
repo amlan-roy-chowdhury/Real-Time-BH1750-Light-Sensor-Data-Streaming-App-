801116d1e1b27e7fc8790cf11ef32b9d858bc361
amlan-roy-chowdhury <arc2233@outlook.com>
2025-04-19 23:35:05 -0400

    Added Grafana SQL Queries

--- RDS_Queries.sql
+++ RDS_Queries.sql
@@ -7,6 +7,10 @@ SELECT * FROM lux_file_summary;
 
 SELECT * FROM lux_file_summary ORDER BY file_date DESC LIMIT 10;
 
+-- View the entire table by order of descending created_at (latest files first)--
+
+SELECT * FROM lux_file_summary ORDER BY created_at DESC LIMIT 10;
+
 
 -- Get file counts per day --
 
@@ -39,3 +43,69 @@ WHERE id NOT IN (
     FROM lux_file_summary
     GROUP BY filename
 );
+
+
+--Grafana Dashboard Queries--
+
+--Today's Min Lux Recorded--
+SELECT MIN(min_lux) AS min_lux
+FROM lux_file_summary
+WHERE file_date = (
+  SELECT MAX(file_date) FROM lux_file_summary
+);
+
+--Today's Max Lux Recorded--
+SELECT MIN(min_lux) AS min_lux
+FROM lux_file_summary
+WHERE file_date = (
+  SELECT MAX(file_date) FROM lux_file_summary
+);
+
+
+--Total Records Today--
+SELECT SUM(record_count) AS total_records
+FROM lux_file_summary
+WHERE file_date = (SELECT MAX(file_date) FROM lux_file_summary);
+
+
+--Daily Summary Table--
+SELECT 
+  filename,
+  TO_CHAR(created_at, 'HH24:MI:SS') AS upload_time,
+  record_count,
+  min_lux,
+  max_lux,
+  avg_lux
+FROM lux_file_summary
+WHERE file_date = (SELECT MAX(file_date) FROM lux_file_summary)
+ORDER BY created_at DESC;
+
+--Min Lux Over Time--
+
+SELECT
+  created_at AS time,
+  min_lux
+FROM lux_file_summary
+ORDER BY created_at;
+
+--Max Lux Over Time--
+SELECT
+  created_at AS time,
+  max_lux
+FROM lux_file_summary
+ORDER BY created_at;
+
+--Average Lux Over Time--
+SELECT
+  created_at AS time,
+  avg_lux
+FROM lux_file_summary
+ORDER BY created_at;
+
+--Latest Average Lux Reading--
+SELECT
+  created_at AS time,
+  avg_lux
+FROM lux_file_summary
+ORDER BY created_at DESC
+LIMIT 1
